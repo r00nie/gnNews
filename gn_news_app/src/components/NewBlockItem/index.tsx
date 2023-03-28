76c00cfa0f0ae7885a12efce81d2b9047bac2d5d
wackomagiciann <banasiakkacperr@gmail.com>
2023-03-28 22:04:04 +0200

    Adding popups to list and blocks

--- gn_news_app/src/components/NewBlockItem/index.tsx
+++ gn_news_app/src/components/NewBlockItem/index.tsx
@@ -1,43 +1,73 @@
-import React from "react";
+import React, {useState} from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
+import {
+    Card,
+    CardContent,
+    CardMedia,
+    Typography,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    Button,
+    Link,
+  } from "@material-ui/core";
 
 type NewsData = {
-    source: {
-        id: string | null;
-        name: string;
-    };
-    author: string | null;
-    title: string;
-    description: string | null;
-    url: string;
-    urlToImage: string | null;
-    publishedAt: string;
-    content: string | null;
+  source: {
+    id: string | null;
+    name: string;
   };
+  author: string | null;
+  title: string;
+  description: string | null;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string | null;
+};
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: "flex",
-      marginBottom: theme.spacing(2),
+      margin: theme.spacing(2),
+      borderRadius: theme.spacing(2),
+      boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+      transition: "transform 0.3s ease-in-out",
+      "&:hover": {
+        transform: "scale(1.05)",
+      },
     },
     media: {
       width: 150,
       height: 150,
       objectFit: "cover",
+      borderRadius: `${theme.spacing(2)}px 0 0 ${theme.spacing(2)}px`,
     },
     content: {
       flex: "1 0 auto",
+      padding: theme.spacing(2),
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
     },
     title: {
-      fontSize: 18,
+      fontSize: 24,
       fontWeight: 500,
+      marginBottom: theme.spacing(2),
+      color: theme.palette.primary.main,
+    },
+    source: {
+      fontSize: 14,
+      color: theme.palette.secondary.main,
       marginBottom: theme.spacing(1),
     },
     description: {
-      fontSize: 14,
+      fontSize: 18,
       color: theme.palette.text.secondary,
+      flexGrow: 1,
     },
   })
 );
@@ -49,7 +79,18 @@ type Props = {
 const NewBlockItem: React.FC<Props> = ({ data }) => {
   const classes = useStyles();
 
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
+    <>
     <Card className={classes.root}>
       {data.urlToImage && (
         <CardMedia
@@ -58,15 +99,40 @@ const NewBlockItem: React.FC<Props> = ({ data }) => {
           title={data.title}
         />
       )}
-      <CardContent className={classes.content}>
-        <Typography className={classes.title} variant="h6">
-          {data.title}
-        </Typography>
-        <Typography className={classes.description} variant="body2">
-          {data.description}
-        </Typography>
+      <CardContent className={classes.content} onClick={handleClickOpen}>
+        <div>
+          <Typography className={classes.source} variant="body2">
+            {`${data.source.name} | ${new Date(data.publishedAt).toLocaleDateString()}`}
+          </Typography>
+          <Typography className={classes.title} variant="h6">
+            {data.title}
+          </Typography>
+          <Typography className={classes.description} variant="body1">
+            {data.description}
+          </Typography>
+        </div>
       </CardContent>
     </Card>
+    <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+    <DialogTitle>{data.title}</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+        {data.content ? data.content : data.description}
+      </DialogContentText>
+      <Typography variant="subtitle1">
+        {`Author: ${data.author ? data.author : "Unknown"}`}
+      </Typography>
+      <Link href={data.url} target="_blank">
+        {data.url}
+      </Link>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleClose} color="primary">
+        Close
+      </Button>
+    </DialogActions>
+  </Dialog>
+  </>
   );
 };
 
--- gn_news_app/src/components/NewListItem/index.tsx
+++ gn_news_app/src/components/NewListItem/index.tsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import { ListItem, ListItemText, ListItemAvatar, Avatar } from "@material-ui/core";
+import {
+    ListItem, 
+    ListItemText, 
+    ListItemAvatar, 
+    Avatar,
+    Typography,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    Button,
+    Link,
+  } from "@material-ui/core";
 
 type NewsData = {
-    source: {
-        id: string | null;
-        name: string;
-    };
-    author: string | null;
-    title: string;
-    description: string | null;
-    url: string;
-    urlToImage: string | null;
-    publishedAt: string;
-    content: string | null;
+  source: {
+    id: string | null;
+    name: string;
   };
-  
+  author: string | null;
+  title: string;
+  description: string | null;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string | null;
+};
+
+type Props = {
+  data: NewsData;
+};
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -26,23 +42,59 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-type Props = {
-  data: NewsData;
-};
-
 const NewsListItem: React.FC<Props> = ({ data }) => {
   const classes = useStyles();
 
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const formattedDate = new Date(data.publishedAt).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+
   return (
-    <ListItem className={classes.root}>
-      <ListItemAvatar>
-        <Avatar>{data.source.name.substring(0, 1)}</Avatar>
-      </ListItemAvatar>
+    <>
+    <ListItem className={classes.root} onClick={handleClickOpen}>
+        {data.urlToImage && (
+            <ListItemAvatar>
+                <Avatar>{data.source.name.substring(0, 1)}</Avatar>
+            </ListItemAvatar>
+        )}
       <ListItemText
         primary={data.title}
-        secondary={data.author || data.source.name}
+        secondary={`${data.source.name} - ${formattedDate}`}
       />
     </ListItem>
+    <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+    <DialogTitle>{data.title}</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+        {data.content ? data.content : data.description}
+      </DialogContentText>
+      <Typography variant="subtitle1">
+        {`Author: ${data.author ? data.author : "Unknown"}`}
+      </Typography>
+      <Link href={data.url} target="_blank">
+        {data.url}
+      </Link>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleClose} color="primary">
+        Close
+      </Button>
+    </DialogActions>
+    </Dialog>
+    </>
   );
 };
 
