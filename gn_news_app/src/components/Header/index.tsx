51cd57c79ab6c1a5a072f717bf6fb8472b3fd993
wackomagiciann <banasiakkacperr@gmail.com>
2023-03-28 13:29:55 +0200

    Creating redux store. Reducer for toggle button

--- gn_news_app/src/components/Header/index.tsx
+++ gn_news_app/src/components/Header/index.tsx
@@ -1,16 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { AppBar, Toolbar, Typography, Link, Button } from '@material-ui/core';
 import { ViewList as ViewListIcon, ViewModule as ViewModuleIcon, Info } from '@material-ui/icons';
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 import HeaderDialog from '../HeaderDialog';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppState } from '../../reducers';
+import { toggleList } from '../../actions';
 
 interface HeaderProps {
   title: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
-
   const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
+  const isList = useSelector((state: AppState) => state.toggleList);
 
   const handleOpen = () => {
     setOpen(true);
@@ -20,8 +24,12 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
     setOpen(false);
   };
 
-  const handleChange = () => {
-    // handle toggle button change
+  const handleChange = (event: React.MouseEvent<HTMLElement>, value: string | null) => {
+    if (value === 'list') {
+      dispatch(toggleList(true));
+    } else {
+      dispatch(toggleList(false));
+    }
   };
 
   return (
@@ -30,7 +38,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
         <Typography variant="h6" component={Link} href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
           {title}
         </Typography>
-        <ToggleButtonGroup orientation="horizontal" exclusive onChange={handleChange} style={{ marginLeft: 'auto', backgroundColor: 'white' }}>
+        <ToggleButtonGroup orientation="horizontal" exclusive value={isList ? 'list' : 'module'} onChange={handleChange} style={{ marginLeft: 'auto', backgroundColor: 'white' }}>
           <ToggleButton value="list" aria-label="list">
             <ViewListIcon />
           </ToggleButton>
@@ -39,7 +47,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
           </ToggleButton>
         </ToggleButtonGroup>
         <Button onClick={handleOpen}>
-          <Info/>
+          <Info />
         </Button>
         <HeaderDialog isOpen={open} onClose={handleClose} />
       </Toolbar>
